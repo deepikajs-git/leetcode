adec5ad2a0e4ffaa1b144211a1b7e6173524cf26
deepikajs-git <deepikajs2007@gmail.com>
2026-06-26 22:02:43 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (18.27%) - LeetSync

---
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.py
@@ -0,0 +1,6 @@
+class Solution(object):
+    def kidsWithCandies(self, candies, extraCandies):
+        maximum = max(candies)
+        return [c + extraCandies >= maximum for c in candies]
+        
+        
