bdf7f8fe60e392df5030d37f49aff9ce2711115a
deepikajs-git <deepikajs2007@gmail.com>
2026-09-19 11:30:39 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (21.56%) - LeetSync

---
+++ 3515-find-if-digit-game-can-be-won/find-if-digit-game-can-be-won.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def canAliceWin(self, nums):
+        single = 0
+        double = 0
+        for x in nums:
+            if x<10:
+                single += x
+            else:
+                double += x
+        return single != double            
